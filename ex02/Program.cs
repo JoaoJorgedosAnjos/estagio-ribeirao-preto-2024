9a2d28033f6608292e47165ac42186103f0f3ead
joaojorge <joaojorgedosanjos@gmail.com>
2024-03-08 20:50:30 -0300

    teste para estagio

---
+++ ex02/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Informe um número para verificar se ele pertence à sequência de Fibonacci: ");
+int number = int.Parse(Console.ReadLine());
+
+if (IsFibonacci(number))
+{
+    Console.WriteLine($"O número {number} pertence à sequência de Fibonacci.");
+}
+else
+{
+    Console.WriteLine($"O número {number} não pertence à sequência de Fibonacci.");
+}
+static bool IsFibonacci(int number)
+    {
+        int firstNumber = 0;
+        int secondNumber = 1;
+
+        while (secondNumber < number)
+        {
+            int nextNumber = firstNumber;
+            firstNumber = secondNumber;
+            secondNumber = nextNumber + secondNumber;
+        }
+
+        return secondNumber == number;
+    }
---
+++ ex05/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Informe uma palavra ou texto para ser invertido:");
+string text = Console.ReadLine();
+string invertedText = "";
+for (int i = text.Length - 1; i >= 0; i--)
+{
+    invertedText += text[i];
+}
+
+Console.WriteLine("A palavra ou texto invertido é: " + invertedText);
